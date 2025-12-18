40b7a1f158fb09aa5d2a1a94de13289907f0704e
Marialetizia-Cristina <marialetiziacristina@gmail.com>
2025-12-18 16:44:37 +0100

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -3,7 +3,6 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App'
 import './i18n'
-import { syncSiteIcons } from './utils/siteIcon'
 
 if (typeof window !== 'undefined') {
   void syncSiteIcons()
