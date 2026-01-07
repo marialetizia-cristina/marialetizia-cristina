9643f55702abb470053817d1084758fad88ac1ca
Niccolò Maffioli <nico.maffioli@gmail.com>
2026-01-07 23:09:34 +0100

    style: adjust Products component layout for improved centering and height

--- src/pages/Products.tsx
+++ src/pages/Products.tsx
@@ -97,7 +97,7 @@ const Products = () => {
 
   return (
     <div style={{
-      padding: 48, textAlign: 'center', fontSize: 28, color: '#222', fontWeight: 700, width: "100%", display: 'flex', alignItems: 'center'
+      padding: 48, textAlign: 'center', fontSize: 28, color: '#222', fontWeight: 700, width: "100%", display: 'flex', alignItems: 'center', height: '80vh', justifyContent: 'center'
     }}>
       Under construction
     </div >
