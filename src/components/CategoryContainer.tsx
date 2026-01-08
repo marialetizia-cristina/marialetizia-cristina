fd61a9bdb366ec9f76ba084ddcd47e95fc13da4b
Niccolò Maffioli <nico.maffioli@gmail.com>
2026-01-08 10:49:48 +0100

    style: conditionally render horizontal rule in CategoryContainer based on current location

--- src/components/CategoryContainer.tsx
+++ src/components/CategoryContainer.tsx
@@ -1,13 +1,18 @@
 import "../style/CategoryContainer.css";
 import CategoryButton from "./CategoryButton";
+
 import { useTranslation } from "react-i18next";
+import { useLocation } from "react-router-dom";
+
 
 const CategoryContainer = () => {
     const { t } = useTranslation();
+    const location = useLocation();
+    const isHome = location.pathname === "/";
 
     return (
         <div className="category-container-wrapper">
-            <hr />
+            {isHome && <hr />}
             <div className="category-container">
                 <CategoryButton name={t("categories.all")?.toUpperCase()} path="/category/all" />
                 <CategoryButton name={t("categories.graphicDesign")?.toUpperCase()} path="/category/graphic-design" />
